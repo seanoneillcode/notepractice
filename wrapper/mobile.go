eb73d64c9dbdd1895e33a25d48337cbb50ae51d2
Sean O'Neill <sean@geckoboard.com>
2026-03-28 14:40:31 +0000

    remove old bits

--- wrapper/mobile.go
+++ wrapper/mobile.go
@@ -6,14 +6,7 @@ import (
 )
 
 func init() {
-	// yourgame.Game must implement ebiten.Game interface.
-	// For more details, see
-	// * https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#Game
 	mobile.SetGame(notepractice.NewGame())
 }
 
-// Dummy is a dummy exported function.
-//
-// gomobile doesn't compile a package that doesn't include any exported function.
-// Dummy forces gomobile to compile this package.
 func Dummy() {}
